487a49c90ba53dcfea57317e6c5d8c7b32f64ec9
bawinsl2 <bawinsl2@ncsu.edu>
2017-01-28 19:15:16 -0500

    stock stuff

--- app/js/index.js
+++ app/js/index.js
@@ -1,5 +1,14 @@
 $(function() {
 
-	$('.stock-quote').stockQuote();	
+
+	$("#stock-submit").click(function(){
+
+		var stock = $("#stock").val();
+		console.log(stock);
+
+		$("#stock-info").html('STOCK: <span class="stock-quote" data-symbol="' + stock + '"></span>');
+		$('.stock-quote').stockQuote();
+
+	})
 
 });
